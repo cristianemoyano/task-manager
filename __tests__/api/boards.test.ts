9e7b579df11c18b6e4e8d09adbfcd8053a147c80
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2022-08-13 14:07:31 +0200

    reworked back end test for boards

--- __tests__/api/boards.test.ts
+++ __tests__/api/boards.test.ts
@@ -1,22 +1,29 @@
 import { testApiHandler } from 'next-test-api-route-handler';
 
+import { connect, closeAndReset } from '../db/utils/reset-db';
+import { newBoard } from '../db/newBoard';
 import boardsHandler from '@/pages/api/boards/index';
 import singleBoardHandler from '@/pages/api/boards/[board_id]';
-import { newBoard } from '../db/newBoard';
+import { IBoard } from '@/typing';
 
-import { connect, closeAndReset } from '../db/utils/reset-db';
+let initialBoards: any[] = [];
 
 beforeAll(async () => {
-  await connect();
+  initialBoards = await connect();
 });
 
 afterAll(async () => {
   closeAndReset();
 });
 
+const user_id = '62f651c7693e2295d9a2d41f';
+
 test('GET /api/boards get correct number and data of all boards ', async () => {
   await testApiHandler({
     handler: boardsHandler,
+    paramsPatcher: (params) => {
+      params.user_id = user_id;
+    },
     test: async ({ fetch }) => {
       const res = await fetch({ method: 'GET' });
       expect(res.status).toBe(200);
@@ -31,6 +38,9 @@ test('GET /api/boards get correct number and data of all boards ', async () => {
 test('POST /api/boards create a new board', async () => {
   await testApiHandler({
     handler: boardsHandler,
+    paramsPatcher: (params) => {
+      params.user_id = user_id;
+    },
     test: async ({ fetch }) => {
       const res = await fetch({
         method: 'POST',
@@ -50,14 +60,15 @@ test('GET /api/boards/[board_id] get a single board', async () => {
   await testApiHandler({
     handler: singleBoardHandler,
     paramsPatcher: (params) => {
-      params.board_id = 3;
+      params.user_id = user_id;
+      params.board_id = initialBoards[1]._id.toString();
     },
     test: async ({ fetch }) => {
       const res = await fetch({ method: 'GET' });
       expect(res.status).toBe(200);
 
       const json = await res.json();
-      expect(json.board.name).toBe('Platform Launch');
+      expect(json.name).toBe('Roadmap');
     },
   });
 });
@@ -66,7 +77,8 @@ test('DELETE /api/boards/[board_id] delete a single board', async () => {
   await testApiHandler({
     handler: singleBoardHandler,
     paramsPatcher: (params) => {
-      params.board_id = 2;
+      params.user_id = user_id;
+      params.board_id = initialBoards[1]._id.toString();
     },
     test: async ({ fetch }) => {
       const res = await fetch({
--- __tests__/db/utils/reset-db.js
+++ __tests__/db/utils/reset-db.js
@@ -3,22 +3,10 @@ import mongoose from 'mongoose';
 import Board from '@/models/boardModel';
 import { initialBoards } from '../initialBoards';
 
-// beforeAll(async () => {
-//   mongoose.connect(process.env.MONGODB_URI);
-
-//   await Board.insertMany(initialBoards);
-// });
-
-// afterAll(async () => {
-//   await Board.deleteMany({});
-
-//   mongoose.connection.close();
-// });
-
 export async function connect() {
   mongoose.connect(process.env.MONGODB_URI);
 
-  await Board.insertMany(initialBoards);
+  return await Board.insertMany(initialBoards);
 }
 
 export async function closeAndReset() {
