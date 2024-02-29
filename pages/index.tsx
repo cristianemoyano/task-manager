5581ed116817b6eaa2e0f5188e6b606d594eeff5
Cristian Moyano <cristianmoyano@MacBook-Pro-de-Cristian.local>
2024-02-29 13:18:52 -0300

    back to redirect

--- pages/index.tsx
+++ pages/index.tsx
@@ -52,14 +52,14 @@ const Home: NextPage<{ boards: IBoard[] }> = ({ boards = [] }) => {
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const session = await getSession(context);
-  // if (!session) {
-  //   return {
-  //     redirect: {
-  //       permanent: false,
-  //       destination: '/register',
-  //     },
-  //   };
-  // }
+  if (!session) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: '/register',
+      },
+    };
+  }
 
   await connectMongo();
 
