8497870bae80682f416a8e363bd650054d3b95ae
Cristian Moyano <cristianmoyano@MacBook-Pro-de-Cristian.local>
2024-03-01 15:34:52 -0300

    improve ui for unasigned

--- components/single_board/BoardTask.tsx
+++ components/single_board/BoardTask.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
 import { ITask } from '@/typing';
+import Image from 'next/image';
 import useModal from '@/contexts/useModal';
 import { ASSIGNEES, OF, SUB_TASKS } from '../constants';
 import { getInitials } from '@/services/utils';
@@ -20,26 +21,44 @@ export default function BoardTask({ task }: { task: ITask }) {
 
   const assigneeColor = task.assignee === "0" ? "bg-gray-400" : "bg-indigo-400"
 
+  const priorityColors = [
+    'bg-gray-300',
+    'bg-yellow-400',
+    'bg-red-500',
+  ]
+
   return (
-    <article
-      className='board__task relative'
-      onClick={() => {
-        setTaskInfosModalContent(task);
-        toggleTaskInfosModal();
-      }}
-    >
-      <h3 className='board__task__title'>{task.title}</h3>
-      <p className='board__task__text'>
-        {SUB_TASKS} ({subtasksCompleted} {OF} {task.subtasks.length})
-      </p>
+    <>
+      <article
+        className='board__task relative'
+        onClick={() => {
+          setTaskInfosModalContent(task);
+          toggleTaskInfosModal();
+        }}
+      >
 
-      {/* ASSIGNEE */}
-      <div className="absolute bottom-0 right-0 mb-4 mr-4">
-        <div className={`w-8 h-8 flex items-center justify-center rounded-full ${assigneeColor} text-white`}>
-          <span className="text-md font-bold">{getInitials(task.assignee === "0" ? "?" : `${ASSIGNEES.find((c) => c._id === task.assignee)?.name}`)}</span>
+        <h3 className='board__task__title'>{task.title}</h3>
+        <p className='board__task__text'>
+          {SUB_TASKS} ({subtasksCompleted} {OF} {task.subtasks.length})
+        </p>
+
+        <div className="grid grid-cols-3 gap-4 mt-3 items-center">
+          <div className="...">
+            {/* Priority */}
+            <div title="Prioridad" className={`w-3 h-3 flex items-center justify-center font-bold  text-white rounded-full ${priorityColors[Number(task.priority)]} text-white`}>
+            </div>
+
+          </div>
+          <div className=""></div>
+          <div className="justify-self-end">
+            <div className={`w-7 h-7 flex items-center justify-center rounded-full ${assigneeColor} text-white`}>
+              <span className="text-sm font-bold">{getInitials(task.assignee === "0" ? "?" : `${ASSIGNEES.find((c) => c._id === task.assignee)?.name}`)}</span>
+            </div>
+          </div>
         </div>
-      </div>
 
-    </article>
+
+      </article>
+    </>
   );
 }
